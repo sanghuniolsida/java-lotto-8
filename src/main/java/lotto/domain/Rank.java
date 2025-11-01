a83239d8d0eb97126f2f3678d610f11313f7ba2e
sanghuniolsida <p0526000@gmail.com>
2025-11-01 20:41:04 +0900

    feat(domain): Rank enum 구현(등수 판정 로직 및 상금 값)

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+public enum Rank {
+    FIRST(2_000_000_000L),
+    SECOND(30_000_000L),
+    THIRD(1_500_000L),
+    FOURTH(50_000L),
+    FIFTH(5_000L),
+    MISS(0L);
+
+    private final long prize;
+
+    Rank(long prize) {
+        this.prize = prize;
+    }
+
+    public static Rank from(int matchNumbersCount, boolean bonusNumberMatched) {
+        if (matchNumbersCount == 6) return FIRST;
+        if (matchNumbersCount == 5 && bonusNumberMatched) return SECOND;
+        if (matchNumbersCount == 5) return THIRD;
+        if (matchNumbersCount == 4) return FOURTH;
+        if (matchNumbersCount == 3) return FIFTH;
+        return MISS;
+    }
+
+    public long prize() {
+        return prize;
+    }
+}
